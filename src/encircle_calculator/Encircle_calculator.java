6053ea2c3e4e10eb159300edd41d145c91d75f3d
deep patel <pateldeep1001@gmail.com>
2019-08-18 10:53:39 -0400

    first commit

---
+++ src/encircle_calculator/Encircle_calculator.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package encircle_calculator;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Deep Patel
+ */
+public class Encircle_calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner inp= new Scanner(System.in);
+        
+    }
+    
+}
